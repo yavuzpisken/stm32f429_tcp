07422f1a52da4e93c176613c0d871648c593c27a
yavuzEv <yavuz.pisken@antsiselektronik.com.tr>
2023-10-06 21:34:10 +0300

    add project

---
+++ tcp/Core/Inc/yvzTcp.h
@@ -0,0 +1,13 @@
+/*
+ * yvzTcp.h
+ *
+ *  Created on: Oct 5, 2023
+ *      Author: camel
+ */
+
+#ifndef INC_YVZTCP_H_
+#define INC_YVZTCP_H_
+
+void tcp_server_init();
+
+#endif /* INC_YVZTCP_H_ */
